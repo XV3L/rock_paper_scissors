6a1e22f0b698185f2962c0c2f4482fbc5681d555
xv3l <x.mejia117@gmail.com>
2023-02-24 14:09:34 -0500

    Added minor styling with CSS and implemented a reset button.

--- app.js
+++ app.js
@@ -13,32 +13,32 @@ function playRound(playerSelection,computerSelection) {
     
     if (playerSelection === 'rock' && computerSelection === 'scissors') {
         playerScore += 1;
-        console.log(`Round won! Player Choice: ${playerSelection} | Computer Choice: ${computerSelection}`)
+        console.log(`Round won! Player Choice: ${playerSelection} | Computer Choice: ${computerSelection}`);
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
         computerScore += 1;
-        console.log(`Round loss! Player Choice: ${playerSelection} | Computer Choice: ${computerSelection}`)
+        console.log(`Round loss! Player Choice: ${playerSelection} | Computer Choice: ${computerSelection}`);
     } else if (playerSelection === 'rock' && computerSelection === 'rock') {
-        console.log(`Draw: ${playerSelection} equals ${computerSelection}!`)
+        console.log(`Draw: ${playerSelection} equals ${computerSelection}!`);
     }
 
     if (playerSelection === 'paper' && computerSelection === 'rock') {
         playerScore += 1;
-        console.log(`Round won! Player Choice: ${playerSelection} | Computer Choice: ${computerSelection}`)
+        console.log(`Round won! Player Choice: ${playerSelection} | Computer Choice: ${computerSelection}`);
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
         computerScore += 1;
-        console.log(`Round loss! Player Choice: ${playerSelection} | Computer Choice: ${computerSelection}`)
+        console.log(`Round loss! Player Choice: ${playerSelection} | Computer Choice: ${computerSelection}`);
     } else if (playerSelection === 'paper' && computerSelection === 'paper') {
-        console.log(`Draw: ${playerSelection} equals ${computerSelection}!`)
+        console.log(`Draw: ${playerSelection} equals ${computerSelection}!`);
     }
 
     if (playerSelection === 'scissors' && computerSelection === 'paper') {
         playerScore += 1;
-        console.log(`Round won! Player Choice: ${playerSelection} | Computer Choice: ${computerSelection}`)
+        console.log(`Round won! Player Choice: ${playerSelection} | Computer Choice: ${computerSelection}`);
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         computerScore += 1;
-        console.log(`Round loss! Player Choice: ${playerSelection} | Computer Choice: ${computerSelection}`)
+        console.log(`Round loss! Player Choice: ${playerSelection} | Computer Choice: ${computerSelection}`);
     } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
-        console.log(`Draw: ${playerSelection} equals ${computerSelection}!`)
+        console.log(`Draw: ${playerSelection} equals ${computerSelection}!`);
     }
 
 };
@@ -51,25 +51,30 @@ function game() {
         console.log(`Player Score: ${playerScore} | Computer Score: ${computerScore}`);
 
     if (playerScore>computerScore) {
-        console.log(`You win! Player Score: ${playerScore} | Computer Score: ${computerScore}`)
+        console.log(`You win! Player Score: ${playerScore} | Computer Score: ${computerScore}`);
     } else if (playerScore<computerScore) {
-        console.log(`You lose! Player Score: ${playerScore} | Computer Score: ${computerScore}`)
-    } else {console.log(`Draw! Player Score: ${playerScore} | Computer Score: ${computerScore}`)}
+        console.log(`You lose! Player Score: ${playerScore} | Computer Score: ${computerScore}`);
+    } else {console.log(`Draw! Player Score: ${playerScore} | Computer Score: ${computerScore}`)};
 
 };
 
 function winner() {
     if (playerScore > 4) {
-        alert('You win! You won 5 rounds before the computer did :)')
+        alert('You win! You won 5 rounds before the computer did :)');
     } else if (computerScore > 4) {
-        alert('You lose! The computer won 5 rounds before you did :(')
-    } else 'error'
+        alert('You lose! The computer won 5 rounds before you did :(');
+    } else 'error';
 };
+
+function newGame() {
+    window.location.reload();
+}
 
 const rock = document.querySelector('.rock');
 const paper = document.querySelector('.paper');
 const scissors = document.querySelector('.scissors');
 const results = document.querySelector('.results');
+const reset = document.querySelector('.reset').addEventListener("click", newGame);
 
 rock.addEventListener('click', () => {
     playRound('rock',getComputerChoice());
